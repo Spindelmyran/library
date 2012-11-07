41de55a25e65445dc58870c1c6d157d83dbc0130
Tony Andersson <spindelmyran@gmail.com>
2012-11-07 13:03:43 +0100

    v1.0

---
+++ server.js
@@ -0,0 +1,3 @@
+var http = require('http'); 
+http.createServer(function (request, response) { response.writeHead(200, {'Content-Type': 'text/html'}); 
+response.end('Woohoozzzzzzzzzzz!'); }).listen(8080);
